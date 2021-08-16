e42deed395d67c82a52ba3ccc3f9e8109e4d6259
Eslam <eslamshaban12328@gmail.com>
2021-08-16 17:48:40 +0200

    social app using firebase

---
+++ lib/modules/chats/chats_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatsScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Center(
+          child: Text('Chats'),
+        ),
+      ),
+    );
+  }
+}

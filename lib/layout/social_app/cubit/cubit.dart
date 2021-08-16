9632ccf005cc890df74e4b20517bce464f37c3ff
Eslam <eslamshaban12328@gmail.com>
2021-08-16 18:57:07 +0200

    social app using firebase

--- lib/layout/social_app/cubit/cubit.dart
+++ lib/layout/social_app/cubit/cubit.dart
@@ -1,8 +1,13 @@
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_app/layout/social_app/cubit/states.dart';
 import 'package:social_app/models/social_user_model.dart';
+import 'package:social_app/modules/chats/chats_screen.dart';
+import 'package:social_app/modules/feeds/feeds_screen.dart';
+import 'package:social_app/modules/settings/settings_screen.dart';
+import 'package:social_app/modules/users/users_screen.dart';
 import 'package:social_app/shared/components/constants.dart';
 
 class SocialCubit extends Cubit<SocialStates> {
@@ -12,24 +17,37 @@ class SocialCubit extends Cubit<SocialStates> {
 
   SocialUserModel model;
 
-  void getUserData()
-  {
+  void getUserData() {
     emit(SocialGetUserLoadingState());
 
-    FirebaseFirestore.instance
-        .collection('users')
-        .doc(uId)
-        .get()
-        .then((value)
-    {
+    FirebaseFirestore.instance.collection('users').doc(uId).get().then((value) {
       //print(value.data());
       model = SocialUserModel.fromJson(value.data());
       emit(SocialGetUserSuccessState());
-    })
-        .catchError((error) {
+    }).catchError((error) {
       print(error.toString());
       emit(SocialGetUserErrorState(error.toString()));
-
     });
+  }
+
+  int currentIndex = 0;
+
+  List<Widget> screens = [
+    FeedsScreen(),
+    ChatsScreen(),
+    UsersScreen(),
+    SettingsScreen(),
+  ];
+
+  List<String> titles = [
+    'Home',
+    'Chats',
+    'Users',
+    'Settings',
+  ];
+
+  void changeBottomNav(int index) {
+    currentIndex = index;
+    emit(SocialChangeBottomNavState());
   }
 }
--- lib/layout/social_app/social_layout.dart
+++ lib/layout/social_app/social_layout.dart
@@ -5,77 +5,47 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_app/layout/social_app/cubit/cubit.dart';
 import 'package:social_app/layout/social_app/cubit/states.dart';
 import 'package:social_app/shared/components/components.dart';
+import 'package:social_app/shared/style/icon_broken.dart';
 
 class SocialLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var cubit = SocialCubit.get(context);
+
     return BlocConsumer<SocialCubit, SocialStates>(
       listener: (context, state) {},
       builder: (context, state) {
         return Scaffold(
           appBar: AppBar(
-            title: Text(
-              'News Feed',
-
-            ),
+            title: Text(cubit.titles[cubit.currentIndex]),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(IconBroken.Search),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(IconBroken.Notification),
+              )
+            ],
           ),
-          body: ConditionalBuilder(
-            condition: SocialCubit.get(context).model != null,
-            builder: (context) {
-              var model = FirebaseAuth.instance.currentUser.emailVerified;
-              print(model);
-
-              return Column(
-                children: [
-                  if (!model)
-                    Container(
-                      color: Colors.amber.withOpacity(.6),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 20.0,
-                        ),
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.info_outline,
-                            ),
-                            SizedBox(
-                              width: 15.0,
-                            ),
-                            Expanded(
-                              child: Text(
-                                'please verify your email',
-                              ),
-                            ),
-                            SizedBox(
-                              width: 15.0,
-                            ),
-                            defaultTextButton(
-                              function: () {
-                                FirebaseAuth.instance.currentUser
-                                    .sendEmailVerification()
-                                    .then((value) {
-                                  showToast(
-                                    text: 'check your mail',
-                                    state: ToastStates.SUCCESS,
-                                  );
-                                }).catchError((error) {});
-                              },
-                              text: 'send',
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  Center(
-                    child: Text('Eslam Shaban',style: TextStyle(fontSize: 25.0,),),
-                  )
-                ],
-              );
+          body: cubit.screens[cubit.currentIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: cubit.currentIndex,
+            onTap: (index) {
+              cubit.changeBottomNav(index);
             },
-            fallback: (context) => Center(child: CircularProgressIndicator()),
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(IconBroken.Home), label: 'Feeds'),
+              BottomNavigationBarItem(
+                  icon: Icon(IconBroken.Chat), label: 'Chats'),
+              BottomNavigationBarItem(
+                  icon: Icon(IconBroken.Location), label: 'Users'),
+              BottomNavigationBarItem(
+                  icon: Icon(IconBroken.Setting), label: 'Settings'),
+            ],
           ),
-
         );
       },
     );

8ec14af3858b5a4d9fa756f0074d3eb0b5bfa7b0
Eslam <eslamshaban12328@gmail.com>
2021-08-23 17:50:45 +0200

    social app using firebase

--- lib/modules/new_post/new_post_screen.dart
+++ lib/modules/new_post/new_post_screen.dart
@@ -1,46 +1,81 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_app/layout/social_app/cubit/cubit.dart';
+import 'package:social_app/layout/social_app/cubit/states.dart';
 import 'package:social_app/shared/components/components.dart';
+import 'package:social_app/shared/style/icon_broken.dart';
 
 class NewPostScreen extends StatelessWidget {
+  var textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    var profileImage = SocialCubit.get(context).profileImage;
-    return Scaffold(
-      appBar: defaultAppBar(
-        context: context,
-        title: 'Create Post',
-        actions: [
-          defaultTextButton(function: () {}, text: 'Post'),
-        ],
-      ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              CircleAvatar(
-                backgroundImage: NetworkImage('${profileImage}'),
-              ),
-              const SizedBox(
-                width: 10.0,
-              ),
-              Column(
-                children: [
-                  Text(
-                    'Eslam Shaban',
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                  Text(
-                    'public',
-                    style: Theme.of(context).textTheme.caption,
-                  ),
-                ],
-              ),
+    return BlocConsumer<SocialCubit, SocialStates>(
+      listener: (context, state) {},
+      builder: (context, listener) {
+        return Scaffold(
+          appBar: defaultAppBar(
+            context: context,
+            title: 'Create Post',
+            actions: [
+              defaultTextButton(function: () {}, text: 'Post'),
             ],
-          )
-        ],
-      ),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 25.0,
+                      backgroundImage: NetworkImage(
+                          'https://image.freepik.com/free-photo/skeptical-woman-has-unsure-questioned-expression-points-fingers-sideways_273609-40770.jpg'),
+                    ),
+                    const SizedBox(width: 15.0),
+                    Expanded(
+                      child: Text(
+                        'Eslam Shaban',
+                        style: TextStyle(height: 1.4),
+                      ),
+                    )
+                  ],
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Whats is on your mind?',
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                //
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: [
+                            Icon(IconBroken.Image),
+                            const SizedBox(width: 5.0),
+                            Text('Add photo'),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                        child:
+                            TextButton(onPressed: () {}, child: Text('#tags')))
+                  ],
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }

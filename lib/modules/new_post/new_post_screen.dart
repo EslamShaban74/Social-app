c0c32727bf55c89c107d9105e67b1a92d0d98ad9
Eslam <eslamshaban12328@gmail.com>
2021-08-23 18:59:17 +0200

    social app using firebase

--- lib/modules/new_post/new_post_screen.dart
+++ lib/modules/new_post/new_post_screen.dart
@@ -13,19 +13,38 @@ class NewPostScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocConsumer<SocialCubit, SocialStates>(
       listener: (context, state) {},
-      builder: (context, listener) {
+      builder: (context, state) {
         return Scaffold(
           appBar: defaultAppBar(
             context: context,
             title: 'Create Post',
             actions: [
-              defaultTextButton(function: () {}, text: 'Post'),
+              defaultTextButton(
+                  function: () {
+                    var now = DateTime.now();
+                    if (SocialCubit.get(context).postImage == null) {
+                      SocialCubit.get(context).createPost(
+                        dateTime: now.toString(),
+                        text: textController.text,
+                      );
+                    } else {
+                      SocialCubit.get(context).uploadPostImage(
+                        dateTime: now.toString(),
+                        text: textController.text,
+                      );
+                    }
+                  },
+                  text: 'Post'),
             ],
           ),
           body: Padding(
             padding: const EdgeInsets.all(20.0),
             child: Column(
               children: [
+                if (state is SocialCreatePostLoadingState)
+                  LinearProgressIndicator(),
+                if (state is SocialCreatePostLoadingState)
+                  const SizedBox(height: 10.0),
                 Row(
                   children: [
                     CircleAvatar(
@@ -51,7 +70,37 @@ class NewPostScreen extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 20.0),
-                //
+                if (SocialCubit.get(context).postImage != null)
+                  Stack(
+                    alignment: AlignmentDirectional.topEnd,
+                    children: [
+                      Container(
+                        height: 300,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4.0),
+                          image: DecorationImage(
+                            image:
+                                FileImage(SocialCubit.get(context).postImage),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          SocialCubit.get(context).removePostImage();
+                        },
+                        icon: CircleAvatar(
+                          radius: 20.0,
+                          child: Icon(
+                            Icons.close,
+                            size: 16.0,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                const SizedBox(height: 20.0),
                 Row(
                   children: [
                     Expanded(
